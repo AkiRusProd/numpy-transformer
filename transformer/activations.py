fixed softmax derivaitive  bug

transformer/activations.py
@@ -8,239 +8,218 @@ References:
 """
 
 
+class Activation(object):
+
+    def forward(self, x):
+        pass
+
+    def backward(self, grad):
+        pass
 
 
-class Sigmoid():
+class Sigmoid(Activation):
 
-    def function(self, x):
+    def forward(self, x):
+        self.x = x
 
         return 1 / (1 + np.exp(-x))
 
-    def derivative(self, x):
-        f_x = self.function(x)
+    def backward(self, grad):
+        f_x = self.forward(self.x)
 
-        return f_x * (1.0 - f_x)
+        return grad * f_x * (1.0 - f_x)
 
 
-class Tanh():
+class Tanh(Activation):
 
-    def function(self, x):
+    def forward(self, x):
+        self.x = x
 
         return np.tanh(x)
 
-    def derivative(self, x):
+    def backward(self, grad):
+        x = self.x
+        return grad * (1.0 - np.power(self.forward(x), 2))
 
-        return 1.0 - np.power(self.function(x), 2)
 
+class Softmax(Activation):
+    def __init__(self) -> None:
+        self.axis = -1
 
-class Softmax():
-    def __init__(self, axis = -1) -> None:
-        self.axis = axis
-
-    def function(self, x):
+    def forward(self, x):
+        self.x = x
         e_x = np.exp(x - np.max(x, axis = self.axis, keepdims=True))
         
         self.softmax =  e_x / np.sum(e_x, axis = self.axis, keepdims=True)
         return self.softmax
 
-    def derivative(self, x):# i=j
-        f_x = self.function(x)
+    # def backward(self, x, grad):# i=j
+    #     f_x = self.forward(x)
         
-        return f_x * (1.0 - f_x)
+    #     return grad * f_x * (1.0 - f_x)
+   
 
-    
-    def jacobian_derivative(self, x, grad = None):
-        batch_size = x.shape[0]
-        softmax = self.function(x)
+    def backward(self, grad = None):
+        #https://e2eml.school/softmax.html
+        #https://suzyahyah.github.io/calculus/machine%20learning/2018/04/04/Jacobian-and-Backpropagation.html
+        #https://sgugger.github.io/a-simple-neural-net-in-numpy.html
+
+        batch_size = self.x.shape[0]
+        softmax = self.forward(self.x)
         
-        J = softmax[:, :, np.newaxis] * np.tile(np.identity(softmax.shape[1]), (softmax.shape[0], 1, 1)) - (softmax[:, np.newaxis, :].transpose(0, 2, 1) @ softmax[:, np.newaxis, :]) #OK FOR DIM = -1 (D, N)
-        # J = softmax[:, :, np.newaxis] * np.tile(np.identity(softmax.shape[1]), (softmax.shape[0], 1, 1)) - np.einsum('ij, ik -> ijk', softmax, softmax) #np.matmul(softmax[:, :, None], softmax[:, None, :])
-        input_grad =  grad[:, np.newaxis, :] @ J
+        J = softmax[..., np.newaxis] * np.tile(np.identity(softmax.shape[-1]), (softmax.shape[0], *np.ones(softmax.ndim, dtype = np.int8))) - (softmax[..., np.newaxis, :].transpose(*np.arange(0, softmax.ndim - 1, 1, dtype=np.int8), -1, -2) @ softmax[..., np.newaxis, :]) #np.matmul(softmax[:, :, None], softmax[:, None, :])
+        input_grad =  grad[..., np.newaxis, :] @ J
         
-        return input_grad.reshape(x.shape) / batch_size
-    # def derivative2(self, x, grad = None): #неправильно отсносительно моей реализации/ х и есть инпут
-    #     #https://e2eml.school/softmax.html
-    #     softmax = self.function(x).reshape(1, -1)
-    #     grad = grad.reshape(1, -1)
-    #     d_softmax = (softmax * np.identity(softmax.size)   #Jacobian matrix                      
-    #                 - softmax.transpose() @ softmax)
-    #     #https://suzyahyah.github.io/calculus/machine%20learning/2018/04/04/Jacobian-and-Backpropagation.html
-    #     input_grad = grad @ d_softmax
-    #     print(d_softmax)
-
-    #     return input_grad.reshape(x.shape)
-
-    # def derivative3(self, x, grad = None):
-    #     softmax = self.function(x)
-    #     s = softmax.reshape(-1,1)
-    #     # print(np.diagflat(s) - np.dot(s, s.T))
-    #     return  (grad.reshape(1, -1) @ (np.diagflat(s) - np.dot(s, s.T))).reshape(x.shape)
+        return input_grad.reshape(self.x.shape) / batch_size
 
 
-    # def derivative5(self, x, grad = None):
-    #     '''Returns the jacobian of the Softmax function for the given set of inputs.
-    #     Inputs:
-    #     x: should be a 2d array where the rows correspond to the samples
-    #         and the columns correspond to the nodes.
-    #     Returns: jacobian
-    #     '''
-    #     s = self.function(x)
-    #     a = np.eye(s.shape[-1])
-    #     temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
-    #     temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
-    #     temp1 = np.einsum('ij,jk->ijk',s,a)
-    #     temp2 = np.einsum('ij,ik->ijk',s,s)
+class LogSoftmax(Activation):
+    def __init__(self) -> None:
+        self.axis = -1
 
-    #     J = temp1 - temp2
-    #     # print(J.shape, grad[:, np.newaxis, :].shape)
-    #     return grad[:, np.newaxis, :] @ J
-
-    # def derivative(self, grad):  i!=j
-    #     #https://sgugger.github.io/a-simple-neural-net-in-numpy.html
-    #     return self.softmax * (grad -(grad * self.softmax).sum(axis=1)[:,None])
-
-class LogSoftmax():
-    def __init__(self, axis = -1) -> None:
-        self.axis = axis
-
-    def softmax_function(self, x):
+    def softmax_forward(self, x):
         e_x = np.exp(x - np.max(x, axis = self.axis, keepdims=True))
         
         self.softmax =  e_x / np.sum(e_x, axis = self.axis, keepdims=True)
         return self.softmax
 
-    def function(self, x):
-        self.log_softmax = np.log(self.softmax_function(x))
+    def forward(self, x):
+        self.x = x
+        self.log_softmax = np.log(self.softmax_forward(x))
         return self.log_softmax
 
-    # def derivative(self, x):# for i==j
-    #     f_x = self.function(x)
+    # def backward(self, x):# for i==j
+    #     f_x = self.forward(x)
         
     #     return (1.0 - f_x)
 
-    def jacobian_derivative(self, x, grad = None): #input (N, D)
-        batch_size = x.shape[0]
-        softmax = self.softmax_function(x)
+    def jacobian_backward(self, grad = None):
+        batch_size = self.x.shape[0]
+        softmax = self.softmax_forward(self.x)
 
-        J = np.tile(np.identity(softmax.shape[1]), (softmax.shape[0], 1, 1)) - (np.ones((x.shape[0], x.shape[1], 1)) @ softmax[:, np.newaxis, :])
+        J = np.tile(np.identity(softmax.shape[-1]), (softmax.shape[0], *np.ones(softmax.ndim, dtype = np.int8))) - (np.ones((*self.x.shape, 1)) @ softmax[..., np.newaxis, :])
         
-        input_grad =  grad[:, np.newaxis, :] @ J
+        input_grad =  grad[..., np.newaxis, :] @ J
         
-        return input_grad.reshape(x.shape) / batch_size
+        return input_grad.reshape(self.x.shape) / batch_size
 
 
-class Softplus():
+class Softplus(Activation):
 
-    def function(self, x):
-        
+    def forward(self, x):
+        self.x = x
         return np.log(1 + np.exp(x))
 
-    def derivative(self, x):
-        
-        return 1 / (1 + np.exp(-x))
+    def backward(self, grad):
+        x = self.x
+        return grad * 1 / (1 + np.exp(-x))
 
-class Softsign():
+class Softsign(Activation):
 
-    def function(self, x):
-        
+    def forward(self, x):
+        self.x = x
         return x / (1 + np.abs(x))
 
-    def derivative(self, x):
+    def backward(self, grad):
+        x = self.x
+        return grad * 1 / np.power(1 + np.abs(x), 2)
 
-        return 1 / np.power(1 + np.abs(x), 2)
-
-class Swish():
+class Swish(Activation):
 
     def __init__(self, beta = 1):
         self.beta = beta
 
-    def function(self, x):
+    def forward(self, x):
+        self.x = x
         self.sigmoid = lambda z: 1 / (1 + np.exp(-z)) 
 
         return x * self.sigmoid(self.beta * x)
 
-    def derivative(self, x):
-        f_x = self.function(x)
+    def backward(self, grad):
+        f_x = self.forward(self.x)
 
-        return self.beta * f_x + self.sigmoid(self.beta * x) * (1 - self.beta * f_x)
+        return grad * (self.beta * f_x + self.sigmoid(self.beta * self.x) * (1 - self.beta * f_x))
 
-class Mish():
+class Mish(Activation):
 
-    def function(self, x):
-
+    def forward(self, x):
+        self.x = x
         return x * np.tanh(np.log(1 + np.exp(x)))
 
-    def derivative(self, x):
-        
-        return np.exp(x) * (4 * (x + 1) + 4 * np.exp(2 * x) + np.exp(3 * x) + np.exp(x) * (4 * x + 6)) / np.power((2 * np.exp(x) + np.exp(2 * x) + 2), 2)
+    def backward(self, grad):
+        x = self.x
+        return grad * (np.exp(x) * (4 * (x + 1) + 4 * np.exp(2 * x) + np.exp(3 * x) + np.exp(x) * (4 * x + 6)) / np.power((2 * np.exp(x) + np.exp(2 * x) + 2), 2))
 
-class TanhExp():
+class TanhExp(Activation):
 
-    def function(self, x):
+    def forward(self, x):
+        self.x = x
 
         return x * np.tanh(np.exp(x))
 
-    def derivative(self, x):
+    def backward(self, grad):
+        x = self.x
+        return grad * (np.tanh(np.exp(x)) - x * np.exp(x) * (np.power(np.tanh(np.exp(x)), 2) - 1))
 
-        return np.tanh(np.exp(x)) - x * np.exp(x) * (np.power(np.tanh(np.exp(x)), 2) - 1)
 
+class ReLU(Activation):
 
-class ReLU():
-
-    def function(self, x):
-
+    def forward(self, x):
+        self.x = x
         return np.maximum(0, x)
 
-    def derivative(self, x):
+    def backward(self, grad):
+        x = self.x
+        return grad * np.where(x <= 0, 0, 1)
 
-        return np.where(x <= 0, 0, 1)
 
-
-class LeakyReLU():
+class LeakyReLU(Activation):
 
     def __init__(self, alpha = 0.01):
         self.alpha = alpha
 
-    def function(self, x):
-
+    def forward(self, x):
+        self.x = x
         return np.where(x <= 0, self.alpha * x, x)
 
-    def derivative(self, x):
+    def backward(self, grad):
+        x = self.x
+        return grad * np.where(x <= 0, self.alpha, 1)
 
-        return np.where(x <= 0, self.alpha, 1)
 
-
-class ELU():
+class ELU(Activation):
 
     def __init__(self, alpha = 0.1):
         self.alpha = alpha 
 
-    def function(self, x):
-
+    def forward(self, x):
+        self.x = x
         return np.where(x <= 0, self.alpha * (np.exp(x) - 1), x)
 
-    def derivative(self, x):
+    def backward(self, grad):
+        x = self.x
+        return grad * np.where(x <= 0, self.alpha + self.forward(x), 1)
 
-        return np.where(x <= 0, self.alpha + self.function(x), 1)
 
-
-class SELU():
+class SELU(Activation):
 
     def __init__(self):
         self.alpha = 1.6732632423543772848170429916717
         self.lmbda = 1.0507009873554804934193349852946 
 
-    def function(self, x):
+    def forward(self, x):
+        self.x = x
         return self.lmbda * np.where(x > 0, x, self.alpha*(np.exp(x)-1))
 
-    def derivative(self, x):
-        return self.lmbda * np.where(x > 0, 1, self.alpha * np.exp(x))
+    def backward(self, grad):
+        x = self.x
+        return grad * self.lmbda * np.where(x > 0, 1, self.alpha * np.exp(x))
 
 
-class GELU():
+class GELU(Activation):
 
-    def function(self, x):
-
+    def forward(self, x):
+        self.x = x
         return (
                 0.5
                 * x
@@ -252,25 +231,26 @@ class GELU():
                 )
             )
 
-    def derivative(self, x):
+    def backward(self, grad):
+        x = self.x
         sech = lambda z: 2 / (np.exp(z) + np.exp(-z))
 
-        return (
+        return grad *(
             0.5 * np.tanh(0.0356774 * np.power(x, 3) + 0.797885 * x)
             + (0.0535161 * np.power(x, 3) + 0.398942 * x)
             * np.power(sech(0.0356774 * np.power(x, 3) + 0.797885 * x), 2)
             + 0.5
         )
 
-class Identity():
+class Identity(Activation):
 
-    def function(self, x):
+    def forward(self, x):
+        self.x = x
+        return x
 
-            return x
-
-    def derivative(self, x):
-
-            return np.ones(x.shape)
+    def backward(self, grad):
+        x = self.x
+        return grad * np.ones(x.shape)
 
     
 activations= {

transformer/layers/base/dense.py
@@ -9,24 +9,16 @@ class Dense():
     ---------------
         Args:
             `units_num` (int): number of neurons in the layer
-            `activation` (str) or (`ActivationFunction` class): activation function
             `use_bias` (bool):  `True` if used. `False` if not used
         Returns:
             output: data with shape (batch_size, units_num)
     """
 
-    def __init__(self, units_num, activation = Identity(), input_shape = None, use_bias = True):
-        # self.units_num   = ValuesChecker.check_integer_variable(units_num, "units_num")
-        # self.input_shape = ValuesChecker.check_input_dim(input_shape, input_dim = 2)
-        # self.activation  = ValuesChecker.check_activation(activation, activations)
-        # self.use_bias    = ValuesChecker.check_boolean_type(use_bias, "use_bias")
+    def __init__(self, units_num, inputs_num = None, use_bias = True):
 
         self.units_num   = units_num
-        self.input_shape = input_shape
-        self.activation  = activation
+        self.inputs_num = inputs_num
         self.use_bias    = use_bias
-
-
         
         self.w = None
         self.b = None
@@ -39,16 +31,14 @@ class Dense():
         self.optimizer = optimizer
 
     def build(self):
-        
-        self.input_size = self.input_shape#[-1]
 
         # self.w = np.random.normal(0, pow(self.input_size, -0.5), (self.input_size, self.units_num))
         #xavier initialization
-        stdv = 1. / np.sqrt(self.input_size)# * 0.5 #input size
+        stdv = 1. / np.sqrt(self.inputs_num)# * 0.5 #input size
         # stdv = np.sqrt(6) / np.sqrt(self.input_size + self.units_num)
         #kaiming initialization
         # stdv = np.sqrt(2 / self.input_size)
-        self.w = np.random.uniform(-stdv, stdv, (self.input_size, self.units_num))
+        self.w = np.random.uniform(-stdv, stdv, (self.inputs_num, self.units_num))
         # if self.use_bias == True:
         #     self.b = np.random.uniform(-stdv, stdv, self.units_num)
         # else:
@@ -77,10 +67,9 @@ class Dense():
 
         self.output_data = np.dot(self.input_data, self.w) + self.b
         
-        return self.activation.function(self.output_data)
+        return self.output_data
 
     def backward(self, error):
-        error *= self.activation.derivative(self.output_data)
         
         self.grad_w = np.sum(np.matmul(self.input_data.transpose(0, 2, 1), error), axis = 0)
         self.grad_b = np.sum(error, axis = (0, 1))

transformer/layers/combined/self_attention.py
@@ -1,8 +1,7 @@
 import numpy as np
 from transformer.layers.base.dense import Dense
-from transformer.layers.base.activation import Activation
 from transformer.layers.base.dropout import Dropout
-from transformer.activations import Softmax
+from transformer.activations import Sigmoid, Softmax
 
 
 class MultiHeadAttention:
@@ -14,12 +13,12 @@ class MultiHeadAttention:
 
         self.d_k, self.d_q, self.d_v = self.d_model // heads_num, self.d_model // heads_num, self.d_model // heads_num #512 / 8 = 64
         
-        self.K_linear = Dense(input_shape = self.d_model, units_num = self.d_k * heads_num, use_bias = False) # self.W_K = np.random.randn(self.d_model, self.d_k)
-        self.Q_linear = Dense(input_shape = self.d_model, units_num = self.d_q * heads_num, use_bias = False) # self.W_Q = np.random.randn(self.d_model, self.d_q)
-        self.V_linear = Dense(input_shape = self.d_model, units_num = self.d_v * heads_num, use_bias = False) # self.W_V = np.random.randn(self.d_model, self.d_v)
-        self.O_linear = Dense(input_shape = self.d_model, units_num = self.d_v * heads_num) # self.W_O = np.random.randn(self.d_model, self.heads_num * self.d_v)
+        self.K_linear = Dense(inputs_num = self.d_model, units_num = self.d_k * heads_num, use_bias = False) # self.W_K = np.random.randn(self.d_model, self.d_k)
+        self.Q_linear = Dense(inputs_num = self.d_model, units_num = self.d_q * heads_num, use_bias = False) # self.W_Q = np.random.randn(self.d_model, self.d_q)
+        self.V_linear = Dense(inputs_num = self.d_model, units_num = self.d_v * heads_num, use_bias = False) # self.W_V = np.random.randn(self.d_model, self.d_v)
+        self.O_linear = Dense(inputs_num = self.d_model, units_num = self.d_v * heads_num) # self.W_O = np.random.randn(self.d_model, self.heads_num * self.d_v)
 
-        self.activation = Activation(Softmax())
+        self.activation = Softmax()
 
         self.dropout = Dropout(dropout)
 

transformer/losses.py
@@ -9,8 +9,8 @@ class MSE():
         return np.power(t - y, 2)
 
     def derivative(self, y, t):
-
-        return -(t - y)
+        
+        return -2 * (t - y) / np.prod(y.shape[1:])
 
 
 class BinaryCrossEntropy():
@@ -45,7 +45,7 @@ class CrossEntropy():
         # self.af = ReLU()
 
     def loss(self, y, t):
-        log_softmax = self.log_softmax.function(y) #np.log(self.softmax.function(y))
+        log_softmax = self.log_softmax.forward(y) #np.log(self.softmax.function(y))
         nll_loss = -log_softmax[np.arange(len(t)), t]
         
         return np.where(t == self.ignore_index, 0, nll_loss)
@@ -55,7 +55,7 @@ class CrossEntropy():
         err = 1/batch_size
         nll_loss_der = -1 * np.where(np.isin(y, y[np.arange(len(t)), t]), err, 0)
        
-        output_err = self.log_softmax.jacobian_derivative(y, nll_loss_der)
+        output_err = self.log_softmax.jacobian_backward(nll_loss_der)
         
         return np.where(t.reshape(-1, 1) == self.ignore_index, 0, output_err)
 

transformer/modules/decoder.py
@@ -1,5 +1,4 @@
 import numpy as np
-from transformer.layers.base.activation import Activation
 from transformer.layers.base.dense import Dense
 from transformer.layers.base.embedding import Embedding
 from transformer.layers.base.dropout import Dropout
@@ -18,11 +17,11 @@ class Decoder:
         for _ in range(layers_num):
             self.layers.append(DecoderLayer(d_model, heads_num, d_ff, dropout))
 
-        self.fc_out = Dense(input_shape = d_model, units_num = trg_vocab_size)
+        self.fc_out = Dense(inputs_num = d_model, units_num = trg_vocab_size)
         self.dropout = Dropout(dropout)
         self.scale = np.sqrt(d_model)
 
-        self.activation = Activation(Identity())
+        self.activation = Identity()#Activation(Identity())
 
 
     def forward(self, trg, trg_mask, src, src_mask):

transformer/test3.py
@@ -1,57 +1,185 @@
-from multiprocessing import Process, Pool
-import time
-import ray
-import numpy as np
-
-ray.init()
-
-# @ray.remote
-def func1(x):
-	print ('func1: starting')
-	for i in range(100):
-		# x[i] *= 2.5353
-		x @ np.random.rand(len(x), len(x))
-	print ('func1: finishing')
-	# return x
-	
-# @ray.remote
-def func2(x):
-	print ('func2: starting')
-	for i in range(100):
-		# x[i] *= x[i]
-		x @ np.random.rand(len(x), len(x))
-	print ('func2: finishing')
-	# return x
-	# print ('func2: finishing')
-
-if __name__ == '__main__':
-	x = np.random.normal(0, 1, (2000, 2000))
-	start_time = time.time()
-	p1 = Process(target=func1, args=(x,))
-	p2 = Process(target=func2, args=(x,))
-	p1.start()
-	p2.start()
-	p1.join()
-	p2.join()
-	# pool = Pool()
-	# result1 = pool.apply_async(func1, [x])    # evaluate "solve1(A)" asynchronously
-	# result2 = pool.apply_async(func2, [x])    # evaluate "solve2(B)" asynchronously
-	# answer1 = result1.get(timeout=10)
-	# answer2 = result2.get(timeout=10)
-	print ('time:', time.time() - start_time)
-
-# x = np.random.normal(0, 1, (2000, 2000))
-# start_time = time.time()
-# # func1(x)
-# # func2(x)
-# ret_id1 = func1.remote(x)
-# ret_id2 = func2.remote(x)
-# ret1, ret2 = ray.get([ret_id1, ret_id2])
-# print ('time:', time.time() - start_time)
+# import sys, os
+# from pathlib import Path
+# sys.path[0] = str(Path(sys.path[0]).parent)
 
 
-x = np.random.normal(0, 1, (2000, 2000))
-start_time = time.time()
-func1(x)
-func2(x)
-print ('time:', time.time() - start_time)
+# import numpy as np
+# from transformer.losses import MSE, BinaryCrossEntropy, CategoricalCrossEntropy, CrossEntropy
+# from transformer.activations import Sigmoid, Softmax, ReLU, LogSoftmax
+# # class Softmax():
+
+# #     def forward(self, x):
+# #         tmp = np.exp(x - np.max(x, axis=1, keepdims=True))
+# #         self.output =  tmp / np.sum(tmp, axis=1, keepdims=True)
+
+# #         return self.output
+
+# #     def backward(self, grad):
+# #         n = np.size(self.output)
+
+# #         tmp = np.tile(self.output, n)
+# #         print(tmp * (np.identity(n) - np.transpose(tmp)).shape)
+# #         return np.dot(tmp * (np.identity(n) - np.transpose(tmp)), grad)
+
+
+# # softmax = Softmax()
+
+# # print(softmax.forward(np.array([[1, 2, 3]])))
+# # print(softmax.backward(np.array([[1, 2, 3]]))
+# # )
+
+
+# # class Softmax():
+
+# #     def function(self, x):
+# #         e_x = np.exp(x - np.max(x, axis = -1, keepdims=True))
+        
+# #         self.softmax =  e_x / np.sum(e_x, axis = -1, keepdims=True)
+# #         return self.softmax
+
+# #     def derivative(self, x):# x <- входные данные итак; i=j
+# #         f_x = self.function(x)
+        
+# #         return f_x * (1.0 - f_x)
+
+# #     def derivative2(self, x, grad = None): #неправильно отсносительно моей реализации/ х и есть инпут
+# #         #https://e2eml.school/softmax.html
+# #         softmax = self.function(x).reshape(1, -1)
+# #         # grad = grad.reshape(1, -1)
+# #         d_softmax = (softmax * np.identity(softmax.size)   #Jacobian matrix                      
+# #                     - softmax.transpose() @ softmax)
+# #         #https://suzyahyah.github.io/calculus/machine%20learning/2018/04/04/Jacobian-and-Backpropagation.html
+# #         # input_grad = grad @ d_softmax
+
+# #         # return input_grad.reshape(x.shape)
+# #         return d_softmax
+
+# #     def derivative3(self, x, grad = None):
+# #         softmax = self.function(x)
+# #         s = softmax.reshape(-1,1)
+# #         # return  (grad.reshape(1, -1) @ (np.diagflat(s) - np.dot(s, s.T))).reshape(x.shape)
+# #         return  np.diagflat(s) - np.dot(s, s.T)
+
+# #     # def derivative(self, grad): неправильно отсносительно моей реализации. град и есть инпут; i!=j
+# #     #     #https://sgugger.github.io/a-simple-neural-net-in-numpy.html
+# #     #     return self.softmax * (grad -(grad * self.softmax).sum(axis=1)[:,None])
+# axis = -1
+
+# #2, 2, 2, 1
+# activation = Softmax(axis)
+# input =  np.array([[1., 2., 3., 4., 5., 6., 7., 8.]]).reshape(2, 2, 2, 1)
+# target = np.array([[1., 1., 0., 1., 0., 1., 0., 1.]]).reshape(2, 2, 2, 1)
+# # grad = np.array([[2, 3, 4]])
+
+# # der1 = softmax.derivative(np.array([[1, 2, 3]]))
+# # print("der1\n", der1)
+# # der2 = softmax.derivative2(np.array([[1, 2, 3]]))
+# # print("der2\n", der2)
+# # der3 = softmax.derivative3(np.array([[1, 2, 3]]))
+# # print("der3\n", der3)
+
+# activation_output = activation.function(input)
+# print("activation output")
+# print(activation_output)
+# mse_loss = MSE()
+# mse_loss_output = mse_loss.loss(activation_output, target)
+# print("mse_loss_output\n", mse_loss_output.mean())
+# mse_loss_derivative = mse_loss.derivative(activation_output, target)
+# print("mse_loss_derivative\n", mse_loss_derivative)
+# # activation_output_derivative = activation.derivative(input) * mse_loss_derivative *2/3
+# activation_output_derivative = activation.jacobian_derivative3(input, mse_loss_derivative) #1D OK
+# print("activation_output_derivative\n", activation_output_derivative)
+
+
+
+
+
+# import torch
+# import torch.nn as nn
+
+# t_input = torch.tensor(input, requires_grad=True)
+# output = nn.Softmax(dim = axis)(t_input) #default dim = -1
+# print("torch activation output")
+# print(output)
+# # log = torch.log(output)
+# loss = nn.MSELoss()
+# torch_loss = loss(output, torch.tensor(target))
+# print(torch_loss)
+# torch_loss.backward()
+# print(t_input.grad)
+
+
+# # print(np.sum(input, axis = 1, keepdims=True))
+# # print(torch.sum(t_input, dim = 1, keepdim=True))
+
+
+
+
+# # print('TEST')
+# # import torch.nn.functional as F
+
+# # a = torch.tensor([1., 2., 3], requires_grad=True)# rand(3, requires_grad=True)
+# # print(a.shape)
+
+# # p = F.softmax(a, dim=0) #mb here dim = 1 while its 0 
+# # # Specify dim dimensions for sfotmax operation
+
+# # print('softmax:', p)
+
+# # print(torch.autograd.grad(p[2], [a])) #jacobian for element
+# # activation = Softmax(axis=1)
+# # print(activation.function(a.detach().numpy().reshape(1, -1)))
+# # # print(activation.derivative(a.detach().numpy()) * a.detach().numpy())
+# # print(activation.derivative2(a.detach().numpy().reshape(1, -1), a.detach().numpy().reshape(1, -1))) #verno
+# # Note that loss must be a value of length 1. Here, since p is a value of dim=1 and length is 3, take p[1].
+
+# # print("tst 2d output")
+# # print(activation.function(input))
+# # print(nn.Softmax(dim = -1)(torch.tensor(input)))
+
+
+
+# # # print(np.diagflat(input, axis = -1))
+# # softmax = activation.function(input).reshape(1, -1)
+# # # grad = grad.reshape(1, -1)
+# # d_softmax = (softmax * np.identity(softmax.size)   #Jacobian matrix                      
+# #             - softmax.transpose() @ softmax)
+
+# # # print("d_softmax\n", d_softmax)
+
+# # # print(softmax * np.identity(softmax.size))
+
+# # softmax = activation.function(input)
+# # d_softmax = softmax[:, :, np.newaxis] * np.tile(np.identity(softmax.shape[1]), (softmax.shape[0], 1, 1)) - softmax.transpose() @ softmax
+# # print(d_softmax)
+# # # print(np.tile(np.identity(softmax.shape[1]), (softmax.shape[0], 1, 1)))
+
+
+# # softmax = np.array([[1, 2, 3], [4, 5, 6]])
+# # # print(softmax.shape)
+# # print(np.einsum('ij, ik -> ijk', softmax, softmax))
+# # print(np.matmul(softmax[:, :, None], softmax[:, None, :]))
+
+
+# # zer_arr = torch.zeros(250, 7600 * 7600)
+
+# # from tempfile import mkdtemp
+# # import os.path as path
+# # filename = path.join(mkdtemp(), 'newfile.dat')
+
+# # fp = np.memmap(filename, dtype='float32', mode='w+', shape=(3,4))
+# # print(fp)
+
+
+# # data = np.arange(12, dtype='float32')
+# # data.shape = (3,4)
+# # fp[:] = data[:]
+# # print(fp)
+
+# # print(fp.filename == path.abspath(filename))
+
+# # fp.flush()
+
+# # newfp = np.memmap(filename, dtype='float32', mode='r', shape=(3,4))
+# # print(newfp)
+# # print(filename)

transformer/transformer.py
@@ -264,9 +264,10 @@ class Transformer():
                
                 _output = output.reshape(output.shape[0] * output.shape[1], output.shape[2])
                 # print(_output)
-                error = self.loss_function.derivative(_output, target_batch[:, 1:].flatten())#[:, np.newaxis]
                 # print(error)
                 loss_history.append(self.loss_function.loss(_output, target_batch[:, 1:].flatten()).mean())#[:, np.newaxis]
+                error = self.loss_function.derivative(_output, target_batch[:, 1:].flatten())#[:, np.newaxis]
+
 
                 self.backward(error.reshape(output.shape))
                 self.update_weights()
@@ -327,22 +328,22 @@ array = np.array([1, 8, 3, 4, 2, 0]).reshape(2, 3)
 array2 = np.array([1, 8, 3, 4, 2, 0]).reshape(2, 3)
 model = Transformer(encoder, decoder, PAD_INDEX)
 #lr = 0.00005; 1e-4
-# def data_gen(V, batch, nbatches):
-#     src_batch = []
-#     trg_batch = []
-#     "Generate random data for a src-tgt copy task."
-#     for i in range(nbatches):
-#         data = np.random.randint(1, V, size=(batch, 6))
-#         data[:, 0] = 1
+def data_gen(V, batch, nbatches):
+    src_batch = []
+    trg_batch = []
+    "Generate random data for a src-tgt copy task."
+    for i in range(nbatches):
+        data = np.random.randint(1, V, size=(batch, 6))
+        data[:, 0] = 1
 
-#         src_batch.append(data)
-#         trg_batch.append(data)
-#     return src_batch, trg_batch
+        src_batch.append(data)
+        trg_batch.append(data)
+    return src_batch, trg_batch
 
-# source, target = data_gen(10, 16, 1)
+# source, target = data_gen(10, 2, 1)
 
 model.compile(optimizer = Adam(alpha = 0.0005), loss_function = CrossEntropy(ignore_index=PAD_INDEX))#alpha = 1e-4, beta=0.9, beta2=0.98, epsilon = 1e-9
-loss_history = model.fit([source[10]], [target[10]], epochs = 10, save_every_epochs = 1, save_path = 'saved models/#2FgS6_transformer')
+loss_history = model.fit([source[10]], [target[10]], epochs = 500, save_every_epochs = 1, save_path = None)
 
 #plot loss history
 def plot_loss_history(loss_history):